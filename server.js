78f6f07691e1f4fb2362a86975fb1863ac78f36f
albert6051 <38640196+albert6051@users.noreply.github.com>
2019-04-01 17:33:41 -0400

    update to newest version

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const session = require('express-session');
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3003;
 process.env.PORT = process.env.PORT || port;
@@ -14,7 +15,8 @@ app.use(bodyParser.json());
 //  Create sessions
 app.use(session({
     secret: 'temporarySecret',
-    saveUnitialized: false
+    saveUnitialized: false,
+    maxAge: 1000 * 60 * 5       //  5 minutes
 }))
 /**
  * 
@@ -35,15 +37,32 @@ app.use(session({
  * 
  * */
 app.use((req, res, next) => {
+    //  req.session.user is resetted after a specific duration of time
+    //  to prevent persistent user sessions
     
-    if (req.session.user === undefined) {
+    const SESSION_DURATION = 1000 * 60 ;     //  1 minute sessions
+    const FIRST_VISIT = req.session.user === undefined;     //  First visit
+    const SESSION_EXPIRED = Date.now() >        //  Session expired
+                                req.session.start + SESSION_DURATION
+    
+    //  Reset session if user is visiting for the first time
+    //  or session has expired
+    if (FIRST_VISIT || SESSION_EXPIRED) {
         
+        //  Remove user info
         req.session.user = {
             user_ID: undefined
         }
         
+        //  Restart session start
+        req.session.start = Date.now();
+        
+    } else if (!SESSION_EXPIRED) {
+        
+        //  Renew session start if still in session
+        req.session.start = Date.now();
+        
     }
-    
     
     next()
     
@@ -53,22 +72,37 @@ app.use((req, res, next) => {
 //Enter:mysql -u nooredin -p -h jobfirstdatabase.c1vr39jujtbs.us-east-2.rds.amazonaws.com
 //Enter:nooredin
 //use mysql;
-const CONNECTION = mysql.createConnection({
-    host: 'jobfirstdatabase.c1vr39jujtbs.us-east-2.rds.amazonaws.com',
-    user: 'nooredin',
-    password: 'nooredin',
-    port: 3306,
-    database: 'job_first'
-})
+
+
+//  Read and parse credentials from file './credentials/db_credentials'
+//  then create a connection to the database
+const CONNECTION = mysql.createConnection(
+    JSON.parse(fs.readFileSync(
+        './credentials/db_credentials', 
+        {encoding: 'utf-8'})))
 
 //  Route handling.
 //  Each handler is in it's own file within ./controllers
-app.get('/', require('./controllers/jobs_controller.js')(CONNECTION));
-app.get('/logout', (req, res) => {
-    delete req.session.user
-    console.log('logout');
-    res.redirect('/login')
+// app.get('/sign_up', require('./controllers/sign_up_controller.js')(CONNECTION));
+// app.get('/sign_up_employee', require('./controllers/sign_up_employee_controller.js')(CONNECTION));
+// app.get('/sign_up_employer', require('./controllers/sign_up_employer_controller.js')(CONNECTION));
+app.get('/sign_up', (req, res) => {
+    res.render('pages/sign_up')
 });
+app.get('/sign_up_employee', (req, res)=> {
+    res.render('pages/sign_up_employee')
+});
+app.get('/sign_up_employer', (req, res)=> {
+    res.render('pages/sign_up_employer')
+});
+app.post('/add_user/Employee', require('./controllers/add_user_employee_controller.js')(CONNECTION));
+app.post('/add_user/Employer', require('./controllers/add_user_employer_controller.js')(CONNECTION));
+
+app.get('/job_des/:job_id', require('./controllers/job_des_controller.js')(CONNECTION));
+app.post('/apply/:job_id', require('./controllers/apply_controller.js')(CONNECTION));
+
+app.get('/', require('./controllers/jobs_controller.js')(CONNECTION));
+app.get('/logout', require('./controllers/logout_controller.js')(CONNECTION));
 app.post('/postjob', require('./controllers/post_job_controller')(CONNECTION))
 
 app.get('/users/:emplid', require('./controllers/get_user.js')(CONNECTION));
